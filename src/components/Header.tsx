e09b84fac94657a9c195eb39ff86334db716dfb1
rin <prettysunshine468@gmail.com>
2022-12-14 20:07:57 +0600

    fix: header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { IoSearch, IoPersonCircleOutline } from "react-icons/io5";
+import logo from "../assets/logo.svg"
 
 const Header = () => {
   return (
-    <div className="bg-gray-500 h-[240px] pt-8 flex">
+    <div className="bg-gray-500 h-[240px] pt-8 flex bg-header bg-no-repeat bg-center bg-cover">
       <div className="max-w-screen-xl mx-auto w-full flex flex-col">
         <div className="flex items-center justify-between">
-          <h1 className="text-white font-bold text-2xl">Megalab</h1>
+          <img src={logo} alt=""/>
           <div className="flex align-center gap-6">
             <IoSearch size="30" fill="white"/>
             <IoPersonCircleOutline size="30" fill="white"/>
