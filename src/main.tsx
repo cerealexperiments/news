a9f6b90a53a3e4c75dd60956184b21771af294df
rin <prettysunshine468@gmail.com>
2022-12-23 10:50:05 +0600

    fix: PostPage component props

--- src/main.tsx
+++ src/main.tsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
 
       {
         path: "/post",
-        element: <PostPage id={1} title={"Заголовок новости"} text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."} datePosted={"29.11.2022"} isLiked={false} image="https://www.pcworld.com/wp-content/uploads/2022/08/dscf0453_final-100797561-orig-1.jpg"/>
+        element: <PostPage />
       },
       {
         path: "/favorites",
