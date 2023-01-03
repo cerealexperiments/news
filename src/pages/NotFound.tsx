6c3de0fdfc36c24212db61466872dae79eecd97c
rin <prettysunshine468@gmail.com>
2023-01-03 20:37:40 +0600

    feat: add NotFound page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="max-w-screen-2xl flex-1 w-full mx-auto flex justify-center items-center">
+      <p className="text-2xl">Страница не найдена</p>
+    </div>
+  );
+};
+
+export default NotFound;
