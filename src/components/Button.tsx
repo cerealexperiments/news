95f8a4ba000fab0fa8f371f72ec565d04a8bf481
rin <prettysunshine468@gmail.com>
2023-01-12 23:55:51 +0600

    fix: button styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,9 +7,9 @@ type ButtonProps = {
   className?: string;
 }
 
-const Button:React.FC<ButtonProps> = ({children, onClick, size= "default", className}) => {
+const Button:React.FC<ButtonProps> = ({children, onClick, size= "default", className = ""}) => {
   return (
-    <button onClick={onClick} className={`${size === "large" ? "py-2.5" : "py-1.5"} ${size === "thin" ? "min-w-[168px]" : "min-w-[128px]"} h-fit min-w-fit w-fit flex justify-center items-center rounded-xl text-white font-medium leading-5 bg-violet-600 hover:bg-violet-700 transition-colors ${className && className}`}>
+    <button onClick={onClick} className={`${size === "large" ? "py-2.5" : "py-1.5"} ${size === "thin" ? "min-w-[168px]" : "min-w-[128px]"} h-fit w-fit flex justify-center items-center rounded-xl text-white font-medium leading-5 bg-violet-600 hover:bg-violet-700 transition-colors ${className}`}>
       {children}
     </button>
   );
